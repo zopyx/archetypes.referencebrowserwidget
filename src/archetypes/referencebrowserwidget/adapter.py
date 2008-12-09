d6c13486c74f63d802bb8532481ac76a5489d579
Joshua LaPlace <josh@enfoldsystems.com>
2008-12-09 02:05:13 +0000

    get values from the form when we have a validation error
    svn path=/MoreFieldsAndWidgets/archetypes.referencebrowserwidget/branches/enfold-v0.9/; revision=10300

--- src/archetypes/referencebrowserwidget/adapter.py
+++ src/archetypes/referencebrowserwidget/adapter.py
@@ -1,8 +1,23 @@
 
+from Products.CMFCore.utils import getToolByName
+
 def ATReferenceAdapter(context, field):
     relationship = field.relationship
-    return [item.getTargetObject()
-            for item in context.getReferenceImpl(relationship)]
+    uid_catalog = getToolByName(context, 'uid_catalog')
+    uids = context.request.form.get(field.getName(), list())
+    objs = list()
+    if type(uids) not in (list, tuple):
+        uids = [uids]
+    for uid in uids:
+        if not uid:
+            continue
+        res = uid_catalog(UID=uid)
+        if res:
+            objs.append(res[0].getObject())
+    if not objs:
+        objs = [item.getTargetObject()
+                for item in context.getReferenceImpl(relationship)]
+    return objs
 
 def ATBackReferenceAdapter(context, field):
     relationship = field.relationship
